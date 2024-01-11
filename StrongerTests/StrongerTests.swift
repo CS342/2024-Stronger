94fe898896f59e03532a8c18f042f292a5d52269
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-01-11 15:53:11 -0800

    Initial Setup

---
+++ StrongerTests/StrongerTests.swift
@@ -0,0 +1,17 @@
+//
+// This source file is part of the Stronger based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import Stronger
+import XCTest
+
+
+class StrongerTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+}
