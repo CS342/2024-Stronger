f0fe7531a524f3b571e4b8486863af43926d42fd
menahassan <34526502+menahassan@users.noreply.github.com>
2024-01-31 19:12:40 -0800

    add reps and bands inputs

---
+++ Stronger/Workout/InputForm.swift
@@ -0,0 +1,37 @@
+//
+//  InputForm.swift
+//  Stronger
+//
+//  Created by Mena Hassan on 1/30/24.
+//
+
+import SwiftUI
+
+struct InputForm: View {
+    @AppStorage("numReps") private var numReps: String = ""
+    @State private var selectedBand: String = "Band 1" // Default selection
+    let bands = ["Band 1", "Band 2", "Band 3", "Band 4", "Band 5"] // Dropdown options
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Number of Reps", text: $numReps)
+
+                // Dropdown Picker
+                Picker("Select Band", selection: $selectedBand) {
+                    ForEach(bands, id: \.self) { option in
+                        Text(option).tag(option)
+                    }
+                }
+            }
+            .navigationBarTitle("Input Results")
+        }
+    }
+}
+
+// Preview
+struct InputForm_Previews: PreviewProvider {
+    static var previews: some View {
+        InputForm()
+    }
+}
