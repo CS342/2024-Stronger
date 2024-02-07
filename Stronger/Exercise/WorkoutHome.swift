3279b0d8d8d0bc7869922348792cc702123be05c
Theodore <ted.kanell@gmail.com>
2024-02-07 15:16:02 -0800

    Connecting workout home

--- Stronger/Exercise/WorkoutHome.swift
+++ Stronger/Exercise/WorkoutHome.swift
@@ -33,13 +33,12 @@ struct WorkoutHome: View {
             
             ForEach(menuItems, id: \.title) { menuItem in
                 HStack {
-                    NavigationLink(destination: WorkoutVideoView()) {
+                    NavigationLink(destination: WorkoutVideoView()){
                         Image( "woman_workout_leg", label: Text("Workout")) .resizable()
                             .aspectRatio(contentMode: .fill)
                             .frame(maxHeight: 180)
                             .clipped()
                     }
-
                     NavigationLink(destination: menuItem.view) {
                         Text(menuItem.title)
                             .foregroundColor(.primary)
@@ -54,15 +53,19 @@ struct WorkoutHome: View {
             }
             
             Spacer()
+            // NavigationLink(destination: HomeReal()) {
+            //             Text("Home")
+            //                 .modifier(NavButton())
+            //         }
             NavigationLink(destination: HomeReal().navigationBarBackButtonHidden(true)) {
                         Text("Home")
                             .modifier(NavButton())
             }
         }
-        .navigationBarTitle("Workout Home")
+        .navigationBarTitle("Workout HomeWorkout Home")
         // .navigationBarBackButtonHidden(true) // Hide the back button on this view
         // .isDetailLink(false)
-        .navigationBarHidden(true) // Hide the navigation bar
+        //.navigationBarHidden(true) // Hide the navigation bar
     }
 }
 
