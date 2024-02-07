fbab7cdf777b77db82e9b9afaa1c8952c28c9b33
Theodore <ted.kanell@gmail.com>
2024-02-07 12:03:32 -0800

    merging

--- Stronger/Exercise/WorkoutHome.swift
+++ Stronger/Exercise/WorkoutHome.swift
@@ -33,10 +33,12 @@ struct WorkoutHome: View {
             
             ForEach(menuItems, id: \.title) { menuItem in
                 HStack {
-                    Image( "woman_workout_leg", label: Text("Workout")) .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(maxHeight: 180)
-                        .clipped()
+                    NavigationLink(destination: menuItem.view) {
+                        Image( "woman_workout_leg", label: Text("Workout")) .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(maxHeight: 180)
+                            .clipped()
+                    }
                     NavigationLink(destination: menuItem.view) {
                         Text(menuItem.title)
                             .foregroundColor(.primary)
