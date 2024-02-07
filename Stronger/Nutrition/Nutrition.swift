645f9c75e3ae2a3676e7bb1c3fac632e1159ae0c
yanavlall <yanav@stanford.edu>
2024-02-07 00:48:05 -0800

    Assignment 2 Changes

---
+++ Stronger/Nutrition/Nutrition.swift
@@ -0,0 +1,23 @@
+//
+//  Nutrition.swift
+//  Stronger
+//
+//  Created by Theodore Kanell on 2/1/24.
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SwiftUI
+
+struct Nutrition: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationBarTitle("Food")
+    }
+}
+
+#Preview {
+    Nutrition()
+}
