ef251a2a646e1bc17045c87b932f0d2de8bf9e37
Tulika Jha <46682005+tulika-jha@users.noreply.github.com>
2024-02-07 23:23:37 -0800

    LLM-based chat window for protein tracking (#28)
    # Add an LLM-based chat window for protein tracking
    
    ## :recycle: Current situation & Problem
    Need to add a chat window for tracking protein intake of users.
    Associated issue: https://github.com/CS342/2024-Stronger/issues/13
    
    ## :gear: Release Notes 
    - Interact with the Pro-Chatbot using a chat-window interface.
    - Helps the user log in their protein intake by computing protein
    content of their meals
    
    ## :books: Documentation
    
    ## :white_check_mark: Testing
    - The prochatbot is being improved on. Currently no tests have been
    added for this feature.
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).
    
    ---------
    
    Co-authored-by: Philipp Zagar <zagar@stanford.edu>

---
+++ Stronger/ProteinTracker/LLMOnboardingView.swift
@@ -0,0 +1,29 @@
+//
+//  LLMOnboardingView.swift
+//  Stronger
+//
+//  Created by Tulika Jha on 01/02/24.
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SpeziLLMOpenAI
+import SpeziOnboarding
+import SwiftUI
+
+struct LLMOnboardingView: View {
+    @Binding var showOnboarding: Bool
+    
+    var body: some View {
+        OnboardingStack(onboardingFlowComplete: !$showOnboarding) {
+            LLMOpenAITokenOnboarding()
+        }
+        .interactiveDismissDisabled(showOnboarding)
+    }
+}
+
+#Preview {
+    LLMOnboardingView(showOnboarding: .constant(false))
+}
