50bc8557f134b3912aa7f94a8a3470082627aaef
kevinvzhu <kevinvzhu@gmail.com>
2024-02-08 01:29:39 -0800

    fixed nav bar duplication

--- Stronger/SavedMeals/SelectNeworSaved.swift
+++ Stronger/SavedMeals/SelectNeworSaved.swift
@@ -8,41 +8,39 @@ import SwiftUI
 
 struct SelectNeworSaved: View {
     var body: some View {
-          NavigationView {
-            VStack {
-                Text("Are you logging a new or saved meal?")
-                    .font(.title)
-                    .bold()
-                    .multilineTextAlignment(.center)
-                    .padding()
-                
-                Divider().frame(height: 2).foregroundColor(.black).padding(.vertical)
-                
-                Spacer()
-                
-                NavigationLink(destination: ChatWindow()) {
-                    Text("New Meal")
-                        .foregroundColor(.black)
-                        .font(.headline)
-                        .padding(.vertical, 20)
-                        .padding(.horizontal, 40)
-                        .background(RoundedRectangle(cornerRadius: 12).stroke(Color.orange, lineWidth: 2))
-                }
-                .padding(.bottom, 70)
-                
-                NavigationLink(destination: SelectSavedMeal()) {
-                    Text("Saved Meal")
-                        .foregroundColor(.black)
-                        .font(.headline)
-                        .padding(.vertical, 20)
-                        .padding(.horizontal, 40)
-                        .background(RoundedRectangle(cornerRadius: 12).stroke(Color.orange, lineWidth: 2))
-                }
-                
-                Spacer()
-          }
-              .navigationBarTitle("", displayMode: .inline)
-              .navigationBarHidden(true)
+        VStack {
+            Text("Are you logging a new or saved meal?")
+                .font(.title)
+                .bold()
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Divider().frame(height: 2).foregroundColor(.black).padding(.vertical)
+            
+            Spacer()
+            
+            NavigationLink(destination: ChatWindow()) {
+                Text("New Meal")
+                    .foregroundColor(.black)
+                    .font(.headline)
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 40)
+                    .background(RoundedRectangle(cornerRadius: 12).stroke(Color.orange, lineWidth: 2))
+            }
+            .padding(.bottom, 70)
+            
+            NavigationLink(destination: SelectSavedMeal()) {
+                Text("Saved Meal")
+                    .foregroundColor(.black)
+                    .font(.headline)
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 40)
+                    .background(RoundedRectangle(cornerRadius: 12).stroke(Color.orange, lineWidth: 2))
+            }
+            
+            Spacer()
+          .navigationBarTitle("", displayMode: .inline)
+          .navigationBarHidden(true)
         }
     }
 }
