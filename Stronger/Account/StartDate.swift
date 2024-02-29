332d776a2bd0ad777a0724887cd6fd3f3b9f3f00
Theodore <ted.kanell@gmail.com>
2024-02-29 00:04:18 -0800

    Still need to change navigation and create week query

--- Stronger/Account/StartDate.swift
+++ Stronger/Account/StartDate.swift
@@ -1,12 +1,9 @@
 //
-// This source file is part of the STRONGER based on the Stanford Spezi Template Application project
-//
-// SPDX-FileCopyrightText: 2023 Stanford University
+// This source file is part of the Spezi open-source project
 //
 // SPDX-License-Identifier: MIT
 //
 
-import Foundation
 import SpeziAccount
 import SpeziFoundation
 import SpeziValidation
@@ -14,71 +11,63 @@ import SpeziViews
 import SwiftUI
 
 
-// Declare a DateFormatter instance
-private let dateFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .medium
-    formatter.timeStyle = .none
-    return formatter
-}()
-
 /// The start date of a user.
 public struct StartDateKey: AccountKey {
     public typealias Value = Date
-    public static let name = LocalizedStringResource("START_DATE")
+    public typealias DataEntry = StartDatePicker
+
+    public static let name = LocalizedStringResource("START_DATE_TITLE")
+
     public static let category: AccountKeyCategory = .personalDetails
+    
+    public static var initialValue: InitialValue<Value> {
+        .empty(Date())
+    }
 }
 
 extension AccountKeys {
-    /// This is the StartDateKey of the user.
+    /// The start date ``AccountKey`` metatype.
     public var startDate: StartDateKey.Type {
         StartDateKey.self
     }
 }
 
+
 extension AccountValues {
-    /// This is the value of the start date to store.
+    /// Access the start date of a user.
     public var startDate: Date? {
         storage[StartDateKey.self]
     }
 }
 
+
 // MARK: - UI
+
 extension StartDateKey {
     public struct DataDisplay: DataDisplayView {
         public typealias Key = StartDateKey
-        private let startDate: Date
-        public init(_ value: Date) {
-            self.startDate = value
-        }
-        public var body: some View {
-            HStack {
-                Text(StartDateKey.name)
-                Spacer()
-                Text("\(startDate, formatter: dateFormatter)")
-                    .foregroundColor(.secondary)
-            }
-            .accessibilityElement(children: .combine)
-        }
-    }
-}
 
-extension StartDateKey {
-    public struct DataEntry: DataEntryView {
-        public typealias Key = StartDateKey
-        @Binding private var startDate: Date
+        private let value: Date
+
+        @Environment(\.locale) private var locale
+
+        private var formatStyle: Date.FormatStyle {
+            .init()
+                .locale(locale)
+                .year(.defaultDigits)
+                .month(locale.identifier == "en_US" ? .abbreviated : .defaultDigits)
+                .day(.defaultDigits)
+        }
+
         public var body: some View {
-            HStack {
-                Text(StartDateKey.name)
-                Spacer()
-                DatePicker(selection: $startDate, in: ...Date(), displayedComponents: .date) {
-                    Text("")
-                }
-                .frame(width: 120) // set frame width to enable more spaces.
+            ListRow(Key.name) {
+                Text(value.formatted(formatStyle))
             }
         }
-        public init(_ value: Binding<Date>) {
-            self._startDate = value
+
+
+        public init(_ value: Date) {
+            self.value = value
         }
     }
 }
