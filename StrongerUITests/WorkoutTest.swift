da1f36cb352e0ceb45331adf58e56f3e339c62fb
Theodore <ted.kanell@gmail.com>
2024-03-05 14:00:11 -0800

    Added some testing

---
+++ StrongerUITests/WorkoutTest.swift
@@ -0,0 +1,38 @@
+//
+// This source file is part of the Stronger based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import XCTest
+
+
+class WorkoutTests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        continueAfterFailure = false
+        
+        let app = XCUIApplication()
+        app.launchArguments = ["--skipOnboarding"]
+        app.launch()
+    }
+    
+    
+    func testApplicationLaunch() throws {
+        let app = XCUIApplication()
+        XCTAssertEqual(app.state, .runningForeground)
+        
+        XCTAssertTrue(app.tabBars["Tab Bar"].buttons["Workout"].waitForExistence(timeout: 2))
+        app.tabBars["Tab Bar"].buttons["Workout"].tap()
+        
+        XCTAssertTrue(app.buttons["Enter Missed Workout"].waitForExistence(timeout: 2))
+//        XCTAssertTrue(app.buttons["Workout 1"].waitForExistence(timeout: 2))
+//        
+//        app.buttons["Weekly Stats"].tap()
+       
+        
+    }
+}
